522cf93e298f2361b346be3b2783b0859aaad60c
2NgonNgon2 <tranvihao40@gmail.com>
2023-01-31 21:36:27 +0700

    upload single file

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,6 @@
 
-import { json } from 'body-parser';
-import { render } from 'ejs';
 import pool from '../configs/connectDB';
+import multer from 'multer';
 
 let getHomepage = async (req,res) => {
     // query database
@@ -43,6 +42,56 @@ let postUpdateUser = async(req,res)=> {
     return res.redirect('/') 
 }
 
+let getUploadFilePage = async(req,res)=> {
+    return res.render('uploadFile.ejs')
+}
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+
+// 'profile_pic' is the name of our file input field in the HTML form
+const upload = multer().single('profile_pic');
+
+let handleUploadFile = async(req,res)=> {
+     upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`  <div style="text-align:center;color:green;width:100%">
+                    <a href="/upload" style="color:green;text-transform:uppercase;text-decoration:none;font-weight:600;font-size:17px">Upload another image</a>
+                    </div>
+                    <hr/>
+                    <div style="text-align:center;">
+                    <img src="/image/${req.file.filename}" width="80%" style="object-fit:cover;max-height:650px;"><hr/>
+                    </div>
+                    `);
+    });
+}
+
+
 module.exports = {//export for other files can use
-    getHomepage ,getDetailPage,createNewUser,deleteUser,getEditPage,postUpdateUser
+    getHomepage ,getDetailPage,createNewUser,deleteUser,getEditPage,postUpdateUser,getUploadFilePage,handleUploadFile
 }
